2624563f8b90fd1fc846c83d79f06b3c1ba31cee
Soulwish <soulwish.ls@gmail.com>
2015-01-05 22:23:11 +0200

    add some models, collections and views. add CNAME file. register domain name. add css styles. add some functionality.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,24 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var order = require('gulp-order');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+    connect.server({
+        middleware: function(connect, opt) {
+            return [
+                function(req, res, next) {
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+                    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+                    // don't just call next() return it
+                    return next();
+                }
+            ];
+        }
+    });
+});
 
 gulp.task('default', function() {
     gulp.src('sass/*.sass')
@@ -17,6 +35,8 @@ gulp.task('default', function() {
         .pipe(order([
             'js/source/underscore.js',
             'js/source/backbone.js',
+            'js/source/firebase.js',
+            'js/source/backbonefire.js',
             'js/source/script.js'
         ], {base: '.'}))
         .pipe(concat('script.js'))
--- js/source/script.js
+++ js/source/script.js
@@ -1,3 +1,65 @@
 $(document).ready(function(){
-    console.log('test');
+    var addQuoteBtn = $('.add-quote'),
+        addQuoteForm = $('.quote-block'),
+        reloadQuoteBtn = $('.reload'),
+        Quote,
+        QuoteCollection,
+        QuoteView,
+        QuotesAppView,
+        collection,
+        app;
+
+    addQuoteBtn.on('click', function(e) {
+        e.preventDefault();
+
+        $(this).toggleClass('add-quote_active');
+
+        addQuoteForm.toggle(300);
+    });
+    
+    Quote = Backbone.Model.extend();
+
+    QuoteCollection = Backbone.Firebase.Collection.extend({
+        model: Quote,
+        url: "https://motoquotes.firebaseio.com"
+    });
+
+    QuoteView = Backbone.View.extend({
+        tagName: 'span',
+        template: _.template("<%= title %>"),
+        initialize: function() {
+            this.listenTo(this.model, "change", this.render);
+        },
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    QuotesAppView = Backbone.View.extend({
+        el: $('.quotes-app'),
+        events: {
+            "click .submit": "createQuote"
+        },
+        initialize: function() {
+            console.log('INIT');
+            this.quote = this.$('.quote');
+            this.quoteInput = this.$('.quote-input');
+            this.secretWordInput = this.$('.secret-word-input');
+        },
+        createQuote: function(e) {
+            e.preventDefault();
+
+            console.log('CREATE');
+            if (!this.quoteInput.val() || !this.secretWordInput.val() || this.secretWordInput.val() !== 'motofamily') { return; }
+
+            this.collection.create({
+                quote: this.quoteInput.val(),
+                time: new Date()
+            });
+        }
+    });
+
+    col = collection = new QuoteCollection();
+    qa = app = new QuotesAppView({collection: collection});
 });
